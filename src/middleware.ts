5b562ad8e99c2127999766196676354b3f318b9e
angel zuniga <angel.zuna@icloud.com>
2023-06-02 06:24:38 -0500

    Added before auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,12 @@ import { NextResponse } from 'next/server'
 export default authMiddleware({
 	
 	publicRoutes: ['/', '/login', '/testing', '/userProfile', '/about' , '/contactus'],
-	// beforeAuth(req, evt) {
-		
-	// },
+	beforeAuth(req, evt) {
+
+		console.log(req.url)
+		console.log(evt.sourcePage)
+		console.log("before auth")
+	},
 	// afterAuth(auth, req) {
 	// 	if (!auth.userId && !auth.isPublicRoute){
 	// 		console.log(auth.userId , auth.isPublicRoute)
