af054a64790fe326848f32391993bca5422a9b5e
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-06-02 04:10:28 +0000

    comented  the after auth method

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,13 +7,14 @@ export default authMiddleware({
 	// beforeAuth(req, evt) {
 		
 	// },
-	afterAuth(auth, req) {
-		if (!auth.userId && !auth.isPublicRoute){
-			const signInUrl = new URL('/testing', req.url)
-			signInUrl.searchParams.set('redirect_url', req.url)
-			return NextResponse.redirect(signInUrl)
-		}
-	},
+	// afterAuth(auth, req) {
+	// 	if (!auth.userId && !auth.isPublicRoute){
+	// 		console.log(auth.userId , auth.isPublicRoute)
+	// 		const signInUrl = new URL('/testing', req.url)
+	// 		signInUrl.searchParams.set('redirect_url', req.url)
+	// 		return NextResponse.redirect(signInUrl)
+	// 	}
+	// },
 })
 
 export const config = {
